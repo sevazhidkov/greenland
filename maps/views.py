411bdc114e457803f0693b145f2fa5310e2edc18
Egor Zhdan <egor.zhdan@gmail.com>
2016-12-30 12:57:48 +0300

    Update template Allow setting time limits for tasks

--- maps/views.py
+++ maps/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def get_task(request):
-    return render(request, 'maps/task.html', {'tasks': [
-        {'title': 'Task', 'bounds': [(59.9172, 30.2919), (59.9366, 30.3337)]}
-    ]})
+    return render(request, 'maps/task.html', {
+        'task': {'title': 'Task1', 'time': 10, 'bounds': [(59.9172, 30.2919), (59.9366, 30.3337)]}
+    })
