46b8a4d0668c764df85f1e8a94672d81dd112beb
Seva Zhidkov <zhidkovseva@gmail.com>
2017-01-01 16:48:10 +0300

    Add API method for question sets list

--- maps/api/views.py
+++ maps/api/views.py
@@ -1,5 +1,17 @@
+import json
 from django.http import HttpResponse
+from maps.models import QuestionSet
 
 
 def list_question_sets(request):
-    return HttpResponse('Lol, udachi')
+    objects = QuestionSet.objects.all()
+    items = []
+    for obj in objects:
+        items.append({
+            'title': obj.title,
+            'max_duration': obj.max_duration.seconds,
+            'creator': {
+                'full_name': obj.creator.get_full_name()
+            }
+        })
+    return HttpResponse(json.dumps(items))
