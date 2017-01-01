7d4ababb54d492ce8593b5f7dd26472f1ff34c35
Seva Zhidkov <zhidkovseva@gmail.com>
2017-01-01 16:06:06 +0300

    Add basic template for api development

--- greenland/urls.py
+++ greenland/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 import maps.views
 
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^choice/', maps.views.get_choice, name='choice'),
     url(r'^task/', maps.views.get_task, name='task'),
+    url(r'^api/', include('maps.api.urls'))
 ]
---
+++ maps/api/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+
+def list_question_sets(request):
+    return HttpResponse('Lol, udachi')
