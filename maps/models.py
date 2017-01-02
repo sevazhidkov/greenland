f84ac3f57c0cd15cb0960f6b5109c61f833f83c7
Seva Zhidkov <zhidkovseva@gmail.com>
2017-01-02 19:15:10 +0300

    Add new migrations

--- maps/models.py
+++ maps/models.py
@@ -42,20 +42,20 @@ class QuestionSet(models.Model):
     title = models.CharField(max_length=100)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL)
     max_duration = models.DurationField()
-    question_ids = JSONTextField()
+    question_ids = JSONTextField(null=True)
 
 
 class AnswerSet(models.Model):
     student = models.ForeignKey(settings.AUTH_USER_MODEL, db_index=True)
-    question_set = models.ForeignKey(QuestionSet)
+    question_set = models.ForeignKey(QuestionSet, null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField(null=True)
 
 
 class Answer(models.Model):
     answer_set = models.ForeignKey(AnswerSet, db_index=True)
-    question_set = models.ForeignKey(QuestionSet)
-    question = models.ForeignKey(Question)
+    question_set = models.ForeignKey(QuestionSet, null=True)
+    question = models.ForeignKey(Question, null=True)
     answer_data = JSONTextField()
     scoring_data = JSONTextField()  # May be recalculated
     duration = models.DurationField()
