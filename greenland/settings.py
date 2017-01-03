f7d2506c8aec877a0c27696d5e5138677c0fd944
Seva Zhidkov <zhidkovseva@gmail.com>
2017-01-03 13:32:26 +0300

    Fix staticfiles serving

--- greenland/settings.py
+++ greenland/settings.py
@@ -126,8 +126,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
