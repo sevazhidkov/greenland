a7fd5a8b1d2624208f16be92e06458226cafc0b0
Egor Zhdan <egor.zhdan@gmail.com>
2017-01-03 10:56:10 +0300

    Fix codestyle

--- maps/control/views.py
+++ maps/control/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def create_map(request):
     if request.method == 'GET':
-         return render(request, 'maps/control/add.html')
+        return render(request, 'maps/control/add.html')
