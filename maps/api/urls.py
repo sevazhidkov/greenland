832d59557dcf2b73797159b4acb96e727492d8fb
ark1 <arkadiy.bazhanov@mail.ru>
2017-01-03 13:34:37 +0300

    Rename API method into question_set_list

--- maps/api/urls.py
+++ maps/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^question_set/', views.http_list_question_sets),
+    url(r'^question_set_list/', views.http_list_question_sets),
     url(r'^answer_set/', views.http_create_answer_set),
     url(r'^answer/', views.create_answer),
     url(r'^question/', views.get_question),
