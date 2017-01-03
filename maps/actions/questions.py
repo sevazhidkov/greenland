e48b416f774d3f5a86c25cafc728c160c2d3b3f8
Egor Zhdan <egor.zhdan@gmail.com>
2017-01-03 16:45:17 +0300

    Display list of questions

--- maps/actions/questions.py
+++ maps/actions/questions.py
@@ -1,27 +1,42 @@
 import math
+import datetime
+import json
+from maps.models import QuestionSet, AnswerSet, Question
 
-from maps.models import QuestionSet, AnswerSet
+
+def pretty_duration(seconds):
+    hr = seconds // 3600
+    res = ''
+    if hr > 0:
+        res += str(hr) + ' hours '
+    seconds -= hr * 3600
+    mn = seconds // 60
+    if mn > 0:
+        res += str(mn) + ' minutes '
+    seconds -= mn * 60
+    if seconds > 0:
+        res += str(seconds) + ' seconds '
+    return res.rstrip()
+
+
+def pretty_display_area(area):
+    return 'N: ' + str(area.north) + ', S: ' + str(area.south) + '<br>' \
+           + 'W: ' + str(area.west) + ', E: ' + str(area.east)
 
 
 def question_set_list():
-    def pretty_duration(seconds):
-        hr = seconds // 3600
-        res = ''
-        if hr > 0:
-            res += str(hr) + ' hours '
-        seconds -= hr * 3600
-        mn = seconds // 60
-        if mn > 0:
-            res += str(mn) + ' minutes '
-        seconds -= mn * 60
-        if seconds > 0:
-            res += str(seconds) + ' seconds '
-        return res.rstrip()
-
     return list(map(lambda q:
                     {'id': q.id, 'title': q.title, 'creator': q.creator.get_full_name(),
                      'duration': pretty_duration(q.max_duration.seconds)},
                     QuestionSet.objects.all()))
+
+
+def question_list():
+    return list(map(lambda q: {'id': q.id, 'statement_data': json.loads(q.statement_data), 'type': q.type,
+                               'duration': pretty_duration(q.max_duration.seconds),
+                               'creator': q.creator.get_full_name(),
+                               'map_area': pretty_display_area(q.map_area.display_area)},
+                    Question.objects.all()))
 
 
 def create_answer_set(user, question_set_id):
